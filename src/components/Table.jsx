65e695e25fc08a51f24af523bae6c265c6df5981
Matthew Creek <matthew.creek27@gmail.com>
2024-05-06 22:29:21 -0500

    ga6 complete

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,15 +1,31 @@
+import './styling.css'
+
 const Table = ({ sat }) => {
+
   return (
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th>Status</th>
      </tr>
      </thead>
      <tbody>
-     <tr>
+      {sat.map((data, id) => {
+        return (
+          <tr key={id}>
+            <td>{data.name}</td>
+            <td>{data.type}</td>
+            <td>{data.launchDate}</td>
+            <td>{data.operational ? 'Active' : 'Inactive'}</td>
+          </tr>
+        )
+      })}
+     {/* <tr>
        <td>Row Data TBD</td>
-     </tr>
+     </tr> */}
      </tbody>
    </table>
   );
